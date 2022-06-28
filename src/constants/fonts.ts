505e5193b8845dee5c9c04edcd90b83c4c1837c7
smvasconcelos <smvasconcelos11@gmail.com>
2022-06-28 00:54:17 -0300

    Updates

--- src/constants/fonts.ts
+++
@@ -1,7 +0,0 @@
-const fonts = {
-	subtitle: "font-family: 'Anton', sans-serif;",
-	title: "font-family: 'Francois One', sans-serif;",
-	text: "font-family: 'Poppins', sans-serif;",
-}
-
-export default fonts;
--- src/constants/localstorage.ts
+++
@@ -1,5 +0,0 @@
-export const localStorageKeys = {
-  connectorStatus: "@token-xyz/connector-status",
-  lang: "@token-xyz/lang",
-  auth: "@token-xyz/auth-token",
-};
